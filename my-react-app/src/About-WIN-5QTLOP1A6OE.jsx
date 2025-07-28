85d5134fdf458c8c1558359dcf02f271d694b7de
CelesteEvertsen <evertsen.celeste@gmail.com>
2025-07-28 17:07:23 +0200

    Tenziz

---
+++ my-react-app/src/About-WIN-5QTLOP1A6OE.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+function About() {
+  return (
+    <main>
+      <div className="about-section">
+        <h2>About</h2>
+        <p>
+          I am a frontend developer with a diverse background in health and
+          technology. I enjoy creating simple, efficient solutions and am always
+          eager to learn new skills and best practices.
+        </p>
+        <h3>Background</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut
+          accusantium laborum vero ea ipsa beatae sunt nostrum at. Reiciendis
+          perferendis recusandae harum hic sapiente fugit voluptas rem repellat
+          optio perspiciatis.
+        </p>
+      </div>
+    </main>
+  );
+}
+export default About;
